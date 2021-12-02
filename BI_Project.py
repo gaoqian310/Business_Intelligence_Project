0528d200b5d263931ebacfb741cf1d00e79c5076
gaoqian310 <68983944+gaoqian310@users.noreply.github.com>
2021-12-02 14:44:41 -0700

    Add files via upload

--- BI_Project.py
+++ BI_Project.py
@@ -23,6 +23,26 @@ df.info()
 df.head(10)
 
 
+# In[23]:
+
+
+import statistics
+data_mean = statistics.mean(df.Sales)
+data_median = statistics.median(df.Sales)
+data_mode = statistics.mode(df.Sales)
+data_max = max(df.Sales)
+data_min = min(df.Sales)
+data_stdev = statistics.stdev(df.Sales)
+
+
+print ("Mean is :", data_mean)
+print ("Median is :", data_median)
+print ("Mode is :", data_mode)
+print ("Maximum Value is :", data_max)
+print ("Minimum Value is :", data_min)
+print ("Standard Deviation is :", data_stdev)
+
+
 # In[ ]:
 
 
